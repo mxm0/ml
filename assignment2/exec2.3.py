ba80f28f0d00946964e8e049a320c60c81981de7
mxm0 <max.innocentini@gmail.com>
2018-04-22 17:32:47 +0200

    Run knn for multiple k values

--- assignment2/exec2.3.py
+++ assignment2/exec2.3.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import cross_val_score, train_test_split 
 from collections import Counter
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import mean_squared_error
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import accuracy_score, mean_squared_error
 
 # Part a - load data
 df = pd.read_csv('housing.csv')
+
 # Part b - Print max, min and their index for each data column.
-'''
 for col in df:
     if col != 'ocean_proximity':
         data = np.array(df[col])
@@ -28,12 +28,11 @@ for col in df:
         plt.xlabel('Value')
         plt.ylabel('Frequency')
         plt.hist([x for x in df[col] if ~pd.isnull(x)])
-'''
+
 # Part c & d - Plotting data
 '''
 From the data plotted it looks like the median income, housing median age and
 median house value data are the closest to a normal distribution.
-'''
 '''
 # Plot latitude/longitude
 plt.figure('GeoMap')
@@ -47,10 +46,10 @@ cm = plt.cm.get_cmap('RdBu')
 plt.scatter(longitudes, latitudes, alpha=0.2, c=house_values, cmap=cm)
 plt.colorbar()
 plt.show()
-'''
+
 # Part e - Splitting data set 80/20-training/test
 train_set, test_set = train_test_split(df, test_size = 0.2, random_state = 42)
-'''
+
 # Recaculting max, min, avg for both training and test dataset
 for train_col, test_col in zip(train_set, test_set):
     if train_col != 'ocean_proximity' or test_col != 'ocean_proximity':
@@ -66,7 +65,7 @@ for train_col, test_col in zip(train_set, test_set):
         print('Max value for', test_col, 'test: ', np.max(test_data))
         print('Min value for', test_col, 'test: ', np.min(test_data))
         print('Avg value for', test_col, 'test: ', np.average(test_data), '\n')
-'''
+
 '''
 Since the mean values seem to be equal or just differ for few points, we can
 assume that the distribution match. Plus plotting an histogram of the two
@@ -87,8 +86,8 @@ y_train = np.array(train_set['median_house_value'])
 y_test = np.array(test_set['median_house_value'])
 X_train = train_set.copy()
 X_test = test_set.copy()
-X_train.drop(['median_house_value', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'ocean_proximity'], axis=1, inplace=True)
-X_test.drop(['median_house_value', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'ocean_proximity'], axis=1, inplace=True)
+X_train.drop(['median_income','median_house_value', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households', 'ocean_proximity'], axis=1, inplace=True)
+X_test.drop(['median_income','median_house_value', 'housing_median_age', 'total_rooms', 'total_bedrooms', 'population', 'households','ocean_proximity'], axis=1, inplace=True)
 
 # Part c - Implement and run KNN
 
@@ -118,32 +117,46 @@ def knn(X_train, y_train, X_test, y_predicted, k):
         current_test_point += 1
         print('Y predicted for test point number', current_test_point, '/', test_point_n)
 
-y_predicted_training = []
-y_predicted_test = []
-k = 3
 
 # Part d - predict house values
+
 # KNN Predict using own implementation (really slow)
-#knn(np.array(X_train)[:1000], y_train, np.array(X_train)[:1000], y_predicted_training, k)
-#knn(np.array(X_train), y_train, np.array(X_test), y_predicted_training, k)
+#knn(np.array(X_train), y_train, np.array(X_train), y_predicted_training, 7)
+#knn(np.array(X_train), y_train, np.array(X_test), y_predicted_test, k)
+#print('For k =', k, 'Training error:', loss_function(y_predicted_training, y_train))
+# Compute test error
+#print('For k =', k, 'Test error:', loss_function(y_predicted_test, y_test))
 
 # KNN Predict using sklearn library
-knn = KNeighborsClassifier(n_neighbors=k)
-knn.fit(np.array(X_train), y_train)
-y_predicted_test = knn.predict(np.array(X_test))
-y_predicted_training = knn.predict(np.array(X_train))
 
-# Compute training error
-print('Training error:', loss_function(y_predicted_training, y_train))
-# Compute test error
-print('Test error:', loss_function(y_predicted_test, y_test))
+k_neighbors = list(range(1, 16))
+
+for k in k_neighbors:
+    y_predicted_test = []
+    y_predicted_training = []
+
+    knn = KNeighborsRegressor(n_neighbors=k)
+    knn.fit(np.array(X_train), y_train)
+    y_predicted_test = knn.predict(np.array(X_test))
+    y_predicted_training = knn.predict(np.array(X_train))
+
+    # Compute training error and test error
+    print('For k =', k, 'Training error:', loss_function(y_predicted_training, y_train), '- Test error:', loss_function(y_predicted_test, y_test))
 
 '''
-For K = 1 - training error = 243519; test error = 4457697671
-For K = 3 - training error = 3584965976; test error = 5261659069
-For k = 5 - training error = 5097664193; test error = 6249786036
-For k = 7 - training error = 6028138988; test error = 6950192896
-For k = 11 - training error = 7150380685; test error = 8368870760
-For k = 13 - training error = 7637452119; test error = 8797348987
-for k = 93 - training error = 14920683644 test error = 15697833417 
+For k = 1 Training error: 1157688439.28 - Test error: 3934862427.7
+For k = 2 Training error: 1294050713.2 - Test error: 3011038938.45
+For k = 3 Training error: 1492765697.91 - Test error: 2755330118.68
+For k = 4 Training error: 1689210720.05 - Test error: 2688112393.12
+For k = 5 Training error: 1833389653.7 - Test error: 2647927295.45
+For k = 6 Training error: 1960164540.69 - Test error: 2638628688.25
+For k = 7 Training error: 2056890537.35 - Test error: 2601429794.2
+For k = 8 Training error: 2142259040.65 - Test error: 2618539152.57
+For k = 9 Training error: 2219563131.23 - Test error: 2636927550.59
+For k = 10 Training error: 2282856818.53 - Test error: 2669452621.89
+For k = 11 Training error: 2340523953.29 - Test error: 2701497007.42
+For k = 12 Training error: 2400187154.89 - Test error: 2749068204.96
+For k = 13 Training error: 2452363231.13 - Test error: 2789885265.78
+For k = 14 Training error: 2495917770.92 - Test error: 2809223293.7
+For k = 15 Training error: 2546859061.11 - Test error: 2829809248.24i
 '''
