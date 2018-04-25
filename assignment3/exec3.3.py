99bd8b6084a340d6c06b8f52378afcdba82c5d54
17innocen <max.innocentini@gmail.com>
2018-04-25 17:53:13 +0200

    Classify digits 3 and 8

--- assignment3/exec3.3.py
+++ assignment3/exec3.3.py
@@ -10,7 +10,7 @@ from sklearn.metrics import mean_squared_error
 train_data = scipy.io.loadmat('usps/usps_train.mat')
 test_data = scipy.io.loadmat('usps/usps_test.mat')
 
-
+'''
 # Part a -  Prepare train and test data for classes 2 & 3
 X_train = np.float64(train_data['train_data'][1000:3000])
 X_test = np.float64(test_data['test_data'][100:300])
@@ -57,6 +57,34 @@ plt.legend()
 plt.ylabel('Mean Squared Error')
 plt.xlabel('K neighbors')
 plt.show()
-
+'''
 #Part d - Classify digit 3 from 8 and compare results
+X_train = np.float64(train_data['train_data'][2000:3000])
+X_test = np.float64(test_data['test_data'][200:300])
+y_train = train_data['train_label'][2000:3000].flatten()
+y_test = test_data['test_label'][200:300].flatten()
+X_train = np.concatenate((X_train, np.float64(train_data['train_data'][7000:8000])))
+X_test = np.concatenate((X_test, np.float64(test_data['test_data'][700:800])))
+y_train = np.concatenate((y_train, train_data['train_label'][7000:8000].flatten()))
+y_test = np.concatenate((y_test, test_data['test_label'][700:800].flatten()))
+
+k_neighbors = [1, 3, 5, 7, 10, 15]
+train_error = []
+test_error  = []
+
+for k in k_neighbors:
+    y_predicted_test = []
+    y_predicted_training = []
+
+    knn = KNeighborsClassifier(n_neighbors = k)
+    knn.fit(X_train, y_train)
+    y_predicted_test = knn.predict(X_test)
+    y_predicted_training = knn.predict(X_train)
+
+    train_err = mean_squared_error(y_train, y_predicted_training)
+    test_err = mean_squared_error(y_test, y_predicted_test)
+
+    print('For k =', k, ', Training error:', train_err ,'- Test error:', test_err)
+    train_error.append(train_err)
+    test_error.append(test_err)
 
